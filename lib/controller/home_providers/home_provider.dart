eb2fd90f8bbe691d5775b445abd7e722ced42cd8
Jabeel Roshan <jabeel@example.com>
2026-02-08 10:27:40 +0530

    initial commit for task

--- lib/controller/home_providers/home_provider.dart
+++ lib/controller/home_providers/home_provider.dart
@@ -1,35 +1,3 @@
-// import 'package:flutter/material.dart';
-//
-// class HomeProvider extends ChangeNotifier {
-//   bool isSearchOpen = false;
-//   final TextEditingController searchController = TextEditingController();
-//
-//   void openSearch() {
-//     isSearchOpen = true;
-//     notifyListeners();
-//   }
-//
-//   void closeSearch() {
-//     isSearchOpen = false;
-//     searchController.clear();
-//     notifyListeners();
-//   }
-//
-//   void toggleSearch() {
-//     if (isSearchOpen) {
-//       closeSearch();
-//     } else {
-//       openSearch();
-//     }
-//   }
-//
-//   @override
-//   void dispose() {
-//     searchController.dispose();
-//     super.dispose();
-//   }
-// }
-
 import 'package:flutter/material.dart';
 
 class HomeProvider extends ChangeNotifier {
